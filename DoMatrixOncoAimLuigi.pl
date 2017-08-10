f1fcf7b48156eaff52ee7c4990dd449b0090fd7c
hongyanli <hongyan.li@singleragenomics.com>
2017-08-09 19:53:10 -0700

    DoMatrixOncoAimLuigi.pl

--- DoMatrixOncoAimLuigi.pl
+++ DoMatrixOncoAimLuigi.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl -w
-¯
+
+#This is only used for Git testing.
+
 die "perl $0 <report.tsv.list> <var.filter.xls>\n" unless (@ARGV ==2);
 open IN,"$ARGV[0]" or die "$ARGV[0] Error!\n";
 open OT,">$ARGV[1]" or die "$ARGV[1] Error!\n";
